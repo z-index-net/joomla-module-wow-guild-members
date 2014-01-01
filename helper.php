f3fdc01c9e8656f77e81cf668dcc9ca7dcbb34fe
b2un0 <branko.wilhelm@gmail.com>
2014-01-01 18:00:55 +0100

    fix: J2.5 JUri

--- helper.php
+++ helper.php
@@ -38,8 +38,14 @@ abstract class ModWowGuildMembersHelper
             return;
         }
 
-        $params->set('guild', rawurlencode(JString::strtolower($params->get('guild'))));
-        $params->set('realm', rawurlencode(JString::strtolower($params->get('realm'))));
+        if (version_compare(JVERSION, 3, '>=')) {
+            $params->set('guild', rawurlencode(JString::strtolower($params->get('guild'))));
+            $params->set('realm', rawurlencode(JString::strtolower($params->get('realm'))));
+        } else {
+            $params->set('realm', str_replace(array('%20', ' '), '-', $params->get('realm')));
+            $params->set('guild', str_replace(array('%20', ' '), '%2520', $params->get('guild')));
+        }
+
         $params->set('region', JString::strtolower($params->get('region')));
         $params->set('lang', JString::strtolower($params->get('lang', 'en')));
         $params->set('link', $params->get('link', 'battle.net'));
