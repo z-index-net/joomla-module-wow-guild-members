0f70516ce349d62394a1227b1aea3e430451c7d3
b2un0 <branko.wilhelm@gmail.com>
2014-03-22 12:34:00 +0100

    enh: new buidld.xml enh: copyright year

--- mod_wow_guild_members.php
+++ mod_wow_guild_members.php
@@ -3,7 +3,7 @@
 /**
  * @author     Branko Wilhelm <branko.wilhelm@gmail.com>
  * @link       http://www.z-index.net
- * @copyright  (c) 2013 Branko Wilhelm
+ * @copyright  (c) 2013 - 2014 Branko Wilhelm
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
